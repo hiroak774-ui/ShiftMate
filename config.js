ddf8bf7c8532cc3684ef892c2c801b91dfb7054b
hiroak774-ui <hiroak774@gmail.com>
2026-08-23 16:57:41 +0900

    Increase API request timeout for shift publishing

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SHIFTMATE_CONFIG = Object.freeze({
   API_URL: "https://script.google.com/macros/s/AKfycbwe1kdpIPUN3Kccwuy0Bm2DuB2ICe4KIOyV7Pts1QDQdXqNFEXCI_TzsXRH52FtsO17AQ/exec",
-  REQUEST_TIMEOUT_MS: 25000
+  REQUEST_TIMEOUT_MS: 60000
 });
